1c51701d2689037b1b1cdb8cbf96135c5d72220a
Nishant Agarwal <agarwal.nishant07@gmail.com>
2014-07-21 10:52:23 +0200

    Added partial fields processor for WCF

---
+++ PartialFields/PartialResponseProcessor.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.IO;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Configuration;
+using System.ServiceModel.Description;
+using System.ServiceModel.Dispatcher;
+using System.Text;
+using System.Web;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace PartialFields
+{
+    /// <summary>
+    /// WCF behavior extension class for processing outgoing message to send partial response.
+    /// </summary>
+    public class PartialResponseProcessor : BehaviorExtensionElement, IDispatchMessageInspector, IEndpointBehavior
+    {
+        #region Public Methods
+
+        #region BehaviorExtensionElement Methods
+
+        /// <inheritDoc />
+        public override Type BehaviorType
+        {
+            get { return typeof(PartialResponseProcessor); }
+        }
+
+        /// <inheritDoc />
+        protected override object CreateBehavior()
+        {
+            return new PartialResponseProcessor();
+        }
+
+        #endregion BehaviorExtensionElement Methods
+
+        #region IDispatchMessageInspector Members
+
+        /// <inheritDoc />
+        public object AfterReceiveRequest(ref System.ServiceModel.Channels.Message request, System.ServiceModel.IClientChannel channel, System.ServiceModel.InstanceContext instanceContext)
+        {
+            return null;
+        }
+
+        /// <inheritDoc />
+        public void BeforeSendReply(ref System.ServiceModel.Channels.Message reply, object correlationState)
+        {
+            string fields = HttpContext.Current.Request.QueryString["fields"];
+
+            if (String.IsNullOrEmpty(fields))
+            {
+                return;
+            }
+
+            reply = ProcessFields(reply, fields);
+        }
+
+        #endregion IDispatchMessageInspector Members
+
+        #region IEndpointBehavior Members
+
+        /// <inheritDoc />
+        public void AddBindingParameters(ServiceEndpoint endpoint, System.ServiceModel.Channels.BindingParameterCollection bindingParameters)
+        {
+            return;
+        }
+
+        /// <inheritDoc />
+        public void ApplyClientBehavior(ServiceEndpoint endpoint, ClientRuntime clientRuntime)
+        {
+            return;
+        }
+
+        /// <inheritDoc />
+        public void ApplyDispatchBehavior(ServiceEndpoint endpoint, EndpointDispatcher endpointDispatcher)
+        {
+            endpointDispatcher.DispatchRuntime.MessageInspectors.Add(this);
+        }
+
+        /// <inheritDoc />
+        public void Validate(ServiceEndpoint endpoint)
+        {
+            return;
+        }
+
+        #endregion IEndpointBehavior Members
+
+        #region Class Methods
+
+        /// <summary>
+        /// Processes the given list of comma separated field names and removes the field values if corresponding field name is not present in the given field list.
+        /// </summary>
+        /// <param name="oldMessage">Original outgoing message.</param>
+        /// <param name="fields">Comma seperated list of fields to be returned.</param>
+        /// <returns>New outgoing message having only the required fields.</returns>
+        public Message ProcessFields(Message oldMessage, string fields)
+        {
+            MemoryStream memoryStream = new MemoryStream();
+            XmlWriter xmlWriter = XmlWriter.Create(memoryStream);
+            oldMessage.WriteMessage(xmlWriter);
+            xmlWriter.Flush();
+            string body = Encoding.UTF8.GetString(memoryStream.ToArray());
+            body = RemoveUtf8ByteOrderMark(body);
+            xmlWriter.Close();
+
+            XMLDocumentHelper xmlHelper = new XMLDocumentHelper();
+
+            XDocument newBody = XDocument.Parse(xmlHelper.GetProcessedXml(body, fields));
+
+            memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(newBody.ToString()));
+            XmlDictionaryReader xdr = XmlDictionaryReader.CreateTextReader(memoryStream, new XmlDictionaryReaderQuotas());
+            Message newMessage = Message.CreateMessage(xdr, int.MaxValue, oldMessage.Version);
+            newMessage.Properties.CopyProperties(oldMessage.Properties);
+            return newMessage;
+
+        }
+
+        /// <summary>
+        /// Removes UTF-8 byte order mark from given XML string.
+        /// </summary>
+        /// <param name="xml">XML string.</param>
+        /// <returns>Given XML string without UTF-8 byte order mark.</returns>
+        public static string RemoveUtf8ByteOrderMark(string xml)
+        {
+            string byteOrderMarkUtf8 = Encoding.UTF8.GetString(Encoding.UTF8.GetPreamble());
+            if (xml.StartsWith(byteOrderMarkUtf8))
+            {
+                xml = xml.Remove(0, byteOrderMarkUtf8.Length);
+            }
+            return xml;
+        }
+
+        #endregion Class Methods
+
+        #endregion Public Methods
+    }
+}
---
+++ PartialFields/XMLDocumentHelper.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace PartialFields
+{
+    /// <summary>
+    /// Helper class for processing XML documents.
+    /// </summary>
+    public class XMLDocumentHelper
+    {
+        public string GetProcessedXml(string xml, string fields)
+        {
+            if (String.IsNullOrEmpty(xml))
+            {
+                return String.Empty;
+            }
+
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(xml);
+            if (doc.GetElementsByTagName("ErrorCode").Count != 0)
+            {
+                return xml;
+            }
+            string rootNodeName = doc.DocumentElement.Name;
+
+            Field field = new Field(rootNodeName, fields.ToLower());
+
+            XmlNode sourceNode = doc.SelectSingleNode(rootNodeName);
+
+            return GetFilteredXml(sourceNode, field);
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sourceNode"></param>
+        /// <param name="field"></param>
+        /// <returns></returns>
+        string GetFilteredXml(XmlNode sourceNode, Field field)
+        {
+            string rootNodeName = sourceNode.Name;
+            string targetNodeName = String.Empty;
+            XmlDocument outdoc = new XmlDocument();
+
+            XmlElement rootNode = outdoc.CreateElement(rootNodeName);
+            foreach (XmlAttribute attribute in sourceNode.Attributes)
+            {
+                rootNode.SetAttribute(attribute.Name, attribute.Value);
+            }
+
+            foreach (XmlNode childNode in sourceNode.ChildNodes)
+            {
+                XmlNode importNode = null;
+                if (childNode.Name != "Metadata")
+                {
+                    importNode = outdoc.ImportNode(childNode, false);
+                    targetNodeName = childNode.Name;
+                }
+                else
+                {
+                    importNode = outdoc.ImportNode(childNode, true);
+                }
+                rootNode.AppendChild(importNode);
+            }
+
+            outdoc.AppendChild(rootNode);
+            XmlNode targetNode = outdoc.SelectSingleNode(String.Format("{0}/{1}", rootNodeName, targetNodeName));
+            sourceNode = sourceNode.SelectSingleNode(targetNodeName);
+
+            if (field.Children.Count > 0)
+            {
+                foreach (Field child in field.Children)
+                {
+                    GetFilteredXml(sourceNode, ref targetNode, child);
+                }
+            }
+
+            if (sourceNode.OwnerDocument.FirstChild.NodeType == XmlNodeType.XmlDeclaration)
+            {
+                XmlDeclaration dec = sourceNode.OwnerDocument.FirstChild as XmlDeclaration;
+                XmlDeclaration xmlDeclaration = outdoc.CreateXmlDeclaration(dec.Version, dec.Encoding, dec.Standalone);
+                outdoc.InsertBefore(xmlDeclaration, outdoc.DocumentElement);
+            }
+
+            return outdoc.OuterXml;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sourceNode"></param>
+        /// <param name="tagetNode"></param>
+        /// <param name="field"></param>
+        void GetFilteredXml(XmlNode sourceNode, ref XmlNode tagetNode, Field field)
+        {
+            foreach (XmlNode childNode in sourceNode.ChildNodes)
+            {
+                if (childNode.Name.ToLower() == field.Name || String.IsNullOrEmpty(field.Name))
+                {
+                    // this node could be single or have multiple child nodes =fields
+                    XmlNode newChildNode = tagetNode.OwnerDocument.CreateNode(XmlNodeType.Element, childNode.Name, "");
+
+                    if (((field.Type == FieldType.Collection)) || (field.Children.Count > 0))
+                    {
+                        // this node we need more depth. so lets go deeper
+                        // get child field
+
+                        if (field.Type == FieldType.Collection)
+                        {
+                            GetFilteredXml(childNode, ref newChildNode, field.Children[0]);
+                        }
+                        else
+                        {
+                            foreach (Field child in field.Children)
+                            {
+                                GetFilteredXml(childNode, ref newChildNode, child);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        // get all what is inside this node added.
+                        newChildNode.InnerXml = childNode.InnerXml;
+                    }
+                    foreach (XmlAttribute attribute in childNode.Attributes)
+                    {
+                        XmlAttribute newAttribute = newChildNode.OwnerDocument.CreateAttribute(attribute.Name);
+                        newAttribute.Value = attribute.Value;
+                        newChildNode.Attributes.Append(newAttribute);
+                    }
+                    tagetNode.AppendChild(newChildNode);
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Entity class for representing XML node as field.
+    /// </summary>
+    public class Field : IComparable<Field>, IEquatable<Field>
+    {
+        /// <summary>
+        /// Gets or sets the name of the field
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the list of children fields.
+        /// </summary>
+        public List<Field> Children { get; set; }
+
+        /// <summary>
+        /// Gets or sets the parent field.
+        /// </summary>
+        public Field Parent { get; set; }
+
+        /// <summary>
+        /// Gets or sets the type of field.
+        /// </summary>
+        public FieldType Type { get; set; }
+
+
+        /// <summary>
+        /// Initializes field object.
+        /// </summary>
+        /// <param name="fieldString"></param>
+        public Field(string fieldName, string ChildfieldString)
+        {
+            // token format is 
+            // field1,field2, field3:group(field31,field32)
+
+            // we can do a a regular expression check before we go ahead.
+
+            this.Name = fieldName;
+
+            int index = 0;
+
+            StringBuilder curFieldName = new StringBuilder();
+
+            Field curField = this;
+
+            this.Children = new List<Field>();
+
+            Field newField = null;
+
+            while (index < ChildfieldString.Length)
+            {
+                char curChar = ChildfieldString[index];
+                curFieldName.Append(curChar);
+
+                switch (curChar)
+                {
+                    case ',':
+                        if (curFieldName.Length > 1)
+                        {
+                            // fieldname complete
+                            curField.Children.Add(new Field(curFieldName.ToString(0, curFieldName.Length - 1)));
+                        }
+                        curFieldName = new StringBuilder(); // empty
+                        break;
+
+                    case ':':
+                        newField = new Field(curFieldName.ToString(0, curFieldName.Length - 1));
+                        curField.Children.Add(newField);
+                        newField.Parent = curField;
+                        curField = newField;
+                        curField.Type = FieldType.Collection;
+                        curFieldName = new StringBuilder(); // empty
+                        break;
+
+                    case '(':
+                        newField = new Field(curFieldName.ToString(0, curFieldName.Length - 1));
+                        curField.Children.Add(newField);
+                        newField.Parent = curField;
+                        curField = newField;
+                        curFieldName = new StringBuilder(); // empty
+                        //field1,field2, field3:group(field31,field32)
+                        break;
+                    case ')':
+                        curFieldName = curFieldName.Replace(")", "");
+                        if (curFieldName.Length > 0)
+                        {
+                            // fieldname complete
+                            curField.Children.Add(new Field(curFieldName.ToString(0, curFieldName.Length)));
+                        }
+
+                        // roll back to parent
+                        curField = curField.Parent;
+                        if (curField.Type == FieldType.Collection)
+                        {
+                            // move one level up
+                            curField = curField.Parent;
+                        }
+                        curFieldName = new StringBuilder(); // empty
+                        break;
+                }
+                index++;
+            }
+            curFieldName = curFieldName.Replace(")", "");
+            if (curFieldName.Length > 0)
+            {
+                curField.Children.Add(new Field(curFieldName.ToString(0, curFieldName.Length)));
+            }
+        }
+
+        public Field(string name)
+        {
+            this.Name = name;
+            this.Children = new List<Field>();
+        }
+
+        public int CompareTo(Field other)
+        {
+            // If other is not a valid object reference, this instance is greater. 
+            if (other == null)
+                return 1;
+            return (this.Name == other.Name) ? 0 : 1;
+        }
+
+        public Field GetChild(string name)
+        {
+            return this.Children.Find(f => f.Name == name);
+        }
+
+        public bool Equals(Field other)
+        {
+            // If other is not a valid object reference, this instance is greater. 
+            if (other == null)
+                return false;
+            return (this.Name == other.Name);
+        }
+    }
+
+    /// <summary>
+    /// Enumeration for field types.
+    /// </summary>
+    public enum FieldType
+    {
+        Simple,
+        Collection
+    }
+
+}
